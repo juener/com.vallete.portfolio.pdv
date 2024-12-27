aeb5613c87fee2a01cb4b05d064813ab49a407c2
Juener <juener@vallete.com>
2024-12-27 06:36:45 +0000

    feat: implement structure for tests/vitest
    Since the basic CRUD is already done, I set up the Vitest and its files
    for e2e tests.

---
+++ backend/src/controllers/users/post-user.e2e-spec.ts
@@ -0,0 +1,28 @@
+import { AppModule } from '@/app.module'
+import { INestApplication } from '@nestjs/common'
+import { Test } from '@nestjs/testing'
+import request from 'supertest'
+
+describe('Create an user (e2e)', () => {
+  let app: INestApplication
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+
+    await app.init()
+  })
+
+  test('[POST] /users', async () => {
+    const response = await request(app.getHttpServer()).post('/users').send({
+      name: 'John Doe',
+      email: 'johndoe@vallete.com',
+      password: '123abc',
+    })
+
+    expect(response.statusCode).toBe(201)
+  })
+})
--- backend/vitest.config.mts
+++ backend/vitest.config.mts
@@ -4,7 +4,6 @@ import tsConfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   test: {
-    include: ['**/*.e2e.spec.ts'],
     globals: true,
     root: './',
   },
